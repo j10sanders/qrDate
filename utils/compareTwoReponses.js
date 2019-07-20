ca0b3bbd54c0f877413ef5d94f1b36fb53b069c8
pakaplace <pleeplace@gmail.com>
2019-07-21 01:17:23 +0200

    added response comparison util

---
+++ utils/compareTwoReponses.js
@@ -0,0 +1,98 @@
+const compareTwoResponses = (response = [], response1 = []) => {
+  if (response.length !== response1.length) {
+    throw Error(
+      "Responses should contain same number of questions and answers."
+    );
+  }
+  let score = 0;
+  let sharedAnswers = [];
+  if (response)
+    response.forEach((answer, i) => {
+      const counterAnswer = response1[i];
+      if (
+        answer.question === counterAnswer.question &&
+        answer.answer === counterAnswer.answer
+      ) {
+        score++;
+        sharedAnswers.push(answer);
+      }
+    });
+  //shared answers will be used to give a random ice breaker question
+  return { score, sharedAnswers };
+};
+
+// compareTwoResponses(response, response1);
+module.exports = compareTwoResponses;
+
+//Example survey and responses
+// const surveyJson = {
+//   surveyFields: {
+//     name: "hello",
+//     status: "Active",
+//     questionsJson: {
+//       data: [
+//         {
+//           question: "Would you rather become...?",
+//           answers: [
+//             "40% Robot",
+//             "Live the rest of your life without technology",
+//             "Technology? I can barely deal with my flip phone"
+//           ]
+//         },
+
+//         {
+//           question: "Your go-to Bodega order",
+//           answers: [
+//             "Bacon Egg and Cheese",
+//             "Jamaican Patty",
+//             "Smoothie",
+//             "Deli Sandwich "
+//           ]
+//         },
+//         {
+//           question: "Which drink do you prefer",
+//           answers: [
+//             "Negroni",
+//             "Margarita",
+//             "A good old beer",
+//             "Glass of wine",
+//             "A joint"
+//           ]
+//         }
+//       ]
+//     }
+//   }
+// };
+
+// // Assumes everyone has the same survey questions
+// const response = [
+//   {
+//     question: "Would you rather become...?",
+//     answer: "Technology? I can barely deal with my flip phone"
+//   },
+
+//   {
+//     question: "Your go-to Bodega order",
+//     answers: "Bacon Egg and Cheese"
+//   },
+//   {
+//     question: "Which drink do you prefer",
+//     answers: "Negroni"
+//   }
+// ];
+
+// const response1 = [
+//   {
+//     question: "Would you rather become...?",
+//     answer: "Technology? I can barely deal with my flip phone"
+//   },
+
+//   {
+//     question: "Your go-to Bodega order",
+//     answers: "Bacon Egg and Cheese"
+//   },
+//   {
+//     question: "Which drink do you prefer",
+//     answers: "Negroni"
+//   }
+// ];
