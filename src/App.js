9c8605017378d95f3c4dc057a21fe9ee99f1cc8f
Jonathan Sanders <jonsandersss@gmail.com>
2019-07-21 22:04:40 -0400

    started a form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Grommet } from 'grommet';
 import { Row, Col, Container } from 'react-bootstrap'
 import ScanOrShow from './scenes/ScanOrShow';
+import PhoneInput from './scenes/TakeQuiz';
 
 const App = () => {
   return (
     <Grommet>
       <Container>
         <Row>
-          <Col xs={6}>
-            <ScanOrShow />
+          <Col sm={{ span: 6, offset: 3 }}>
+            <PhoneInput />
+            {/* <ScanOrShow /> */}
           </Col>
         </Row>
       </Container>
