00f8a51ea106c1fa04b8547b3d99c9b872fe3e6c
pakaplace <pleeplace@gmail.com>
2019-08-12 18:34:37 -0400

    added gifs for three categories

--- src/utils/getGif.js
+++ src/utils/getGif.js
@@ -1,10 +1,40 @@
+const yesGifs = [
+  "https://media.giphy.com/media/iTjhKpMF50fNS/giphy.gif",
+  "https://media.giphy.com/media/cXKZDaAdEgcqA/giphy.gif",
+  "https://media.giphy.com/media/fxHpSohzG4uK4/giphy.gif",
+  "https://media.giphy.com/media/u23zXEvNsIbfO/giphy.gif",
+  "https://media.giphy.com/media/y8Mz1yj13s3kI/giphy.gif",
+  "https://media.giphy.com/media/QMkPpxPDYY0fu/giphy.gif",
+  "https://media.giphy.com/media/3kK1hN0rsPVVGWuJR7/giphy.gif",
+  "https://media.giphy.com/media/l1ughbsd9qXz2s9SE/giphy.gif"
+];
+
+const maybeGifs = [
+  "https://media.giphy.com/media/1BcfiGuO4Nxe1KlTe9/giphy.gif",
+  "https://media.giphy.com/media/FCzMUL20U7LkA/giphy.gif",
+  "https://media.giphy.com/media/69EeWAwQgFyTMDZAIZ/giphy.gif",
+  "https://media.giphy.com/media/WSO1ZT9sug15C/giphy.gif",
+  "https://media.giphy.com/media/DfdbTJZx6Yjra/giphy.gif",
+  "https://media.giphy.com/media/3oEduV4SOS9mmmIOkw/giphy.gif"
+];
+
+const negativeGifs = [
+  "https://media.giphy.com/media/12h4pgk1SRtLuo/giphy.gif",
+  "https://media.giphy.com/media/zZyPxf3Cz58uk/giphy.gif",
+  "https://media.giphy.com/media/g0vZyAfPuhVd2SPj6q/giphy.gif",
+  "https://media.giphy.com/media/3o7TKHluf42s3st6qA/giphy.gif",
+  "https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/giphy.gif",
+  "https://media.giphy.com/media/KJ2jDqNON6mZ2/giphy.gif",
+  "https://media.giphy.com/media/1jWAOgDVDXfp96hz8f/giphy.gif"
+];
+
 export default rating => {
-  switch(true) {
-    case (rating > .9):
-      return "https://media.giphy.com/media/l2QE6naM1MTLEiK1W/source.gif"
-    case (rating > .5):
-      return "https://media.giphy.com/media/3oriO6qJiXajN0TyDu/source.gif"
+  switch (true) {
+    case rating > 0.6:
+      return yesGifs[Math.floor(Math.random() * yesGifs.length)];
+    case rating > 0.3:
+      return maybeGifs[Math.floor(Math.random() * maybeGifs.length)];
     default:
-      return "https://media.giphy.com/media/p0RDMJGgMXF96/source.gif"
+      return negativeGifs[Math.floor(Math.random() * negativeGifs.length)];
   }
-}
+};
