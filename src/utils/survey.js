ad4bb7929d61ee67679776cfe3d54b510fbfebfb
pakaplace <pleeplace@gmail.com>
2019-08-20 22:04:18 -0400

    cleaned up survey

--- src/utils/survey.js
+++ src/utils/survey.js
@@ -1,19 +1,15 @@
 export default {
   data: [
     {
-      question: "Would you rather become...?",
-      answers: [
-        "40% Robot",
-        "Live the rest of your life without technology",
-        "Technology? I can barely deal with my flip phone"
-      ]
+      question: "Would you rather become ...",
+      answers: ["40% Robot", "Live the rest of your life without technology"]
     },
     {
-      question: "On a Friday night out, you can find me...",
+      question: "On a Friday night out, you can find me ...",
       answers: [
-        "Dancing at a Brooklyn Night Club",
-        "A Jazz Bar",
-        "A homey bar",
+        "Dancing at a Brooklyn night club",
+        "A play or performance of some sort",
+        "My neighborhood bar",
         "In bed"
       ]
     },
@@ -25,7 +21,7 @@ export default {
       ]
     },
     {
-      question: "My future midlife crisis would be...",
+      question: "Your future midlife crisis would be ...",
       answers: [
         "Return to the land and start a farm in Upstate New York",
         "Have an affair",
@@ -34,55 +30,54 @@ export default {
       ]
     },
     {
-      question: "What is your love language?",
+      question: "How do you express love?",
       answers: [
         "Quality Time",
         "Acts of Service",
         "Words of Affirmation",
         "Gifts",
         "Physical Touch",
-        "Can people stop bringing up love languages? Mine change by the day"
+        "Can people stop bringing up love languages? Mine changes by the day"
       ]
     },
     {
-      question: "My idea of the perfect pet is ___? ",
+      question: "Your idea of the perfect pet is ... ",
       answers: [
-        "Anything loyal, fluffy, and loving",
+        "Something loyal, fluffy, and loving",
+        "Something low maintenance",
         "Anything I can dress up and fit in my bag",
-        "Anything low maintenance that can survive a few days if I forget to feed it",
-        "A pet?! I can barely take care of myself"
+        "I can barely take care of myself"
       ]
     },
     {
-      question: "My view on relationships is ___",
+      question: "Your view on relationships is ...",
       answers: [
         "They are either meant to be or they’re not",
         "They require constant work and revisiting of joint goals",
-        "They save me from having to make awkward conversation at dinner parties and weddings"
+        "They save me from having to make awkward conversation at dinner parties"
       ]
     },
 
     {
       question:
-        "If you’ve just been given a one time gift of $10,000, you would____",
+        "If you’ve just been given a one time gift of $10,000, you would ...",
       answers: [
         "Donate it to a noteworthy cause",
-        "Put it in the bank or invest it. Planning for the future is important",
-        "Spend it and enjoy; there’s no time like the present",
-        "Buy shots for everyone at this bar"
+        "Save or invest it. Planning for the future is important",
+        "Spend it and enjoy; there’s no time like the present"
       ]
     },
     {
-      question: "My idea of the perfect Sunday is _____",
+      question: "Your idea of the perfect Sunday is ...",
       answers: [
         "Getting up early and checking items off my to do list",
-        "Going on an adventure, which may involve exploring a new part of town or going on a trip",
+        "Going on an adventure- exploring a new part of town or going on a trip",
         "Lounging in pajamas all day and watching reruns",
-        "Brunch! It’s Sunday Funday!"
+        "Boozy Brunch"
       ]
     },
     {
-      question: "I’m looking for ________",
+      question: "You are looking for ...",
       answers: [
         "A life-long partner",
         "A casual date",
@@ -92,71 +87,61 @@ export default {
     },
 
     {
-      question: "On the weekdays, my alarm ______ ",
+      question: "On the weekdays, your alarm ... ",
       answers: [
-        "Is set to a time between 6 and 8am. It depends if I have an early morning meeting",
+        "Is set between 6 and 8am.",
         "Is set at 10-minute intervals for an hour, because I like to hit snooze",
         "Goes off five minutes before I need to be out the door",
         "Is nonexistent. I don’t usually have anywhere to be until late afternoon."
       ]
     },
     {
-      question: "The holidays makes me feel _________ ",
+      question: "The holidays makes you feel ... ",
       answers: [
-        "Anxious. There are a lot of people that I have been avoiding since the last family gathering",
-        "Excited. I can’t wait for a reason for everyone I love to be in one place",
+        "Anxious. There are a lot of people I've been avoiding since the last family gathering",
+        "Excited. I can’t wait to see my loved ones",
         "Stressed. I usually have trouble balancing family time with work",
         "Lonely, because even though I want to, I am often unable to travel home"
       ]
     },
     {
-      question: "The last time I prayed was  _______ ",
+      question: "The last time you prayed was ... ",
       answers: [
         "Last weekend at my church, temple, mosque, etc.",
         "During the major holidays; I’m only religious at certain points during the year",
-        "Never. I’m not religious, but consider myself to be spiritual ",
+        "Never. I’m not religious, but consider myself to be spiritual",
         "Right now. Find me a match!"
       ]
     },
     {
-      question: "A job should be _________________ ",
+      question: "A job should be ...",
       answers: [
-        "Last weekend at my church, temple, mosque, etc.",
-        "During the major holidays; I’m only religious at certain points during the year",
-        "Never. I’m not religious, but consider myself to be spiritual ",
-        "Right now. Find me a match!"
+        "Lucrative",
+        "Mission driven",
+        "Provide flexibility",
+        "Fun and social"
       ]
     },
     {
-      question: "My perfect partner ________.",
+      question: "Your perfect partner ...",
       answers: [
         "Is always by my side",
-        "Does his/her own thing, but is always home to tuck me in at night",
+        "Does their own thing, but is there when I need them",
         "Knows how to balance time with me and time with friends",
-        "Is not usually around"
+        "Mostly does their own thing"
       ]
     },
     {
-      question: "My perfect vacation is ________.",
+      question: "Your perfect vacation is ...",
       answers: [
         "Something adventurous (i.e., a climbing, skiing, etc.)",
         "Something relaxing (i.e., a beach, or somewhere poolside)",
-        "Something educational (i.e., a trip packed with cultural experiences and language or cooking classes)",
+        "Something educational (i.e., cultural experiences like a language or cooking class)",
         "Anywhere that doesn’t involve a plane"
       ]
     },
     {
-      question:
-        "If at the door of the plane when sky diving, I want my partner to...",
-      answers: [
-        "Be ready to jump with me",
-        "Push me out, because even though I wanted to do this, I sometime get nervous when it’s time to act",
-        "Wake me up! This is an insane nightmare. Why am I here?"
-      ]
-    },
-
-    {
-      question: "Family is ________",
+      question: "Family is ...",
       answers: [
         "Everything. I try to see my relative as much as possible.",
         "A once a year obligation",
@@ -164,42 +149,42 @@ export default {
       ]
     },
     {
-      question: "Kids are ________",
+      question: "Kids are ...",
       answers: [
-        "Amazing. I would like to have a large family.",
-        "Great, I would like to have one or two. ",
-        "Not on the radar now, but up for consideration if I find the right partner.",
-        "Not something that I would ever consider."
+        "Amazing. I would like to have a large family",
+        "Great, I would like to have one or two",
+        "Not on the radar now, but up for consideration if I find the right partner",
+        "Not something that I would ever consider"
       ]
     },
     {
-      question: "When making decisions, I...",
+      question: "When making decisions, you ...",
       answers: [
         "Consult my horoscope ",
         "Carefully weigh the pros and cons of each option",
         "Go with my gut instinct",
-        "Run it by my mom "
+        "Run it by my mom"
       ]
     },
     {
-      question: "When it comes to learning, I believe ________",
+      question: "When it comes to learning, you believe ...",
       answers: [
         "We should constantly seek knowledge, as we are all lifelong learners",
-        "That we should study whatever we need to help us get ahead",
-        "That it’s best left in the past with formal schooling"
+        "We should study whatever we need to help us get ahead",
+        "It’s best left in the past with formal schooling"
       ]
     },
     {
-      question: "When it comes to money, my partner and I ________",
+      question: "When it comes to money, you and your partner ...",
       answers: [
         "Should split everything down the middle for the duration of our relationship",
-        "Should pay for our own expenses and negotiate when it comes to items, we purchase together",
+        "Should pay for our own expenses and negotiate on joint purchases",
         "Should combine our money and pay from a joint account ",
-        "Have decided that the person who earns more will pay for everything"
+        "Have decided that the person who earns more will pay for most things"
       ]
     },
     {
-      question: "I envision my wedding as being ________",
+      question: "You envision your wedding as ...",
       answers: [
         "A large, traditional, family affair",
         "A celebration focusing on our friends",
@@ -209,8 +194,7 @@ export default {
     },
 
     {
-      question:
-        "When it comes to politics, I am interested in someone ________",
+      question: "When it comes to politics, you are interested in someone ...",
       answers: [
         "Who is active and involved in grassroots efforts",
         "Who keeps up to date with the news and has an informed, educated opinion",
@@ -219,13 +203,13 @@ export default {
     },
     {
       question:
-        "In 10 years, with regard to my love life, I will most like be ________",
+        "In 10 years, with regard to your love life, you will most like be ...",
       answers: [
         "Casually dating- there are so many fun people to meet!",
-        "Married with children and doing the whole white picket fence in the suburbs thing",
+        "Married with children; somewhere with more space",
         "Married with children but still hustling in the city",
-        "Married and no kids- enjoying my time with my partner",
-        "at this event! When day will it be in 2029?"
+        "Married but no kids- enjoying time with my partner",
+        "I have no idea"
       ]
     }
   ]
